4c144f01319f1f60eb8980ee18df6a21a9644ca4
Mustapha Botchway <muscoff2008@gmail.com>
2025-03-17 15:10:08 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const cors = require('cors')
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 4000
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+
+app.get('/', (req, res)=>{
+    res.json({message: 'Welcome to Docman Backend API verison@1.0.0'})
+})
+
+const server = http.createServer(app)
+
+server.listen(port, ()=>console.log(`Backend app is running on http://localhost:${port}`))
